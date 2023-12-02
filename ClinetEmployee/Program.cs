81cf19902ccffed8db1a75447903bbfbed414e7f
Mohammedmassod <mohammedmassowd@gmail.com>
2023-12-02 03:32:09 +0300

    #Code organization #Organizing the presentation

--- ClinetEmployee/Program.cs
+++ ClinetEmployee/Program.cs
@@ -1,10 +1,6 @@
-﻿using RestSharp;
-using System;
+﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Net.Http.Json;
 using System.Net.Http;
-using System.Text;
 using System.Threading.Tasks;
 using Newtonsoft.Json;
 
@@ -12,227 +8,188 @@ namespace ClinetEmployee
 {
     public class Program
     {
+        private static readonly HttpClient client = new HttpClient();
+
         static async Task Main(string[] args)
         {
-            //start-show
+            bool continueExecution = true;
 
-            /*   HttpClient client = new HttpClient();
-               var responseTask = client.GetAsync("https://localhost:7101/api/Contact");
-               responseTask.Wait();
-               if (responseTask.IsCompleted)
-               {
-                   var result = responseTask.Result;
-                   if (result.IsSuccessStatusCode)
-                   {
-                       var massegeTask = result.Content.ReadAsStringAsync();
-                       massegeTask.Wait();
-                       Console.WriteLine("massege for WebApi" + massegeTask.Result);
-                       Console.ReadLine();
-
-                   }
-
-               }*/
-            //حذف عنصر بارقم
-            /* int idToDelete = 1004; // تعيين قيمة الـ ID التي ترغب في حذفها
-
-             HttpClient client = new HttpClient();
-             var responseTask = client.DeleteAsync($"https://localhost:7101/api/Contact?id={idToDelete}");
-             responseTask.Wait();
-
-             if (responseTask.IsCompleted)
-             {
-                 var result = responseTask.Result;
-                 if (result.IsSuccessStatusCode)
-                 {
-                     Console.WriteLine($"Data with ID {idToDelete} deleted successfully");
-                     // أي تعليمات أو شيء آخر بعد حذف البيانات
-                 }
-                 else
-                 {
-                     Console.WriteLine($"Failed to delete data with ID {idToDelete}. Status code: {result.StatusCode}");
-                     // إذا كان هناك أي سبب لفشل الحذف يمكنك إضافة تعليمات هنا
-                 }
-             }
-             Console.ReadLine(); // هذا السطر يجبر البرنامج على الانتظار قبل الإغلاق
- */
-            //نهاية كود الحذف بالرقم
-
-
-            //بداية كود انشاء <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
-
-            // البيانات الجديدة التي تريد إدخالها
-            /*  var newContact = new
-              {
-                  contactId = 1,
-                  firstName = "ali",
-                  lastName = "ali",
-                  email = "ali@ali.com",
-                  phoneNumber = "123456789"
-              };
-
-              string url = "https://localhost:7101/api/Contact"; // عنوان الخدمة
-
-              // يحول الكائن إلى JSON ويقوم بإرساله إلى الخدمة
-              var content = new StringContent(System.Text.Json.JsonSerializer.Serialize(newContact), System.Text.Encoding.UTF8, "application/json");
-
-              using (var client = new HttpClient())
-              {
-                  var response = await client.PostAsync(url, content);
-
-                  if (response.IsSuccessStatusCode)
-                  {
-                      Console.WriteLine("Data added successfully");
-                  }
-                  else
-                  {
-                      Console.WriteLine("Add operation failed");
-                  }
-              }
-
-              Console.ReadLine(); // هذا السطر يجبر البرنامج على الانتظار قبل الإغلاق
-  */
-            //نهاية كود انشا>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
-
-
-            //بداية كود التعديل 
-
-            var client = new HttpClient();
-            var contact = new Contact
+            while (continueExecution)
             {
-                // قم بتحديث البيانات التي ترغب في تعديلها في كائن Contact هنا
-                ContactId = 1007, // تحديد رقم الاتصال الذي تريد تعديله
-                FirstName = "ali",
-                LastName = "nasser",
-                Email = "ali@ali.com",
-                PhoneNumber = "774785445"
-                // يمكنك تحديث الحقول الأخرى إذا كانت بياناتك تتضمن ذلك
+                Console.WriteLine("Choose an action:");
+                Console.WriteLine("1. Add");
+                Console.WriteLine("2. Delete");
+                Console.WriteLine("3. Update");
+                Console.WriteLine("4. Retrieve");
+                Console.WriteLine("5. RetrieveAllContacts");
+                Console.WriteLine("6. Exit");
+                int choice = int.Parse(Console.ReadLine());
+
+                switch (choice)
+                {
+                    case 1:
+                        await AddContact();
+                        break;
+                    case 2:
+                        await DeleteContact();
+                        break;
+                    case 3:
+                        await UpdateContact();
+                        break;
+                    case 4:
+                        await RetrieveContact();
+                        break;
+                    case 5:
+                        await RetrieveAllContacts();
+                        break;
+                    case 6:
+                        continueExecution = false;
+                        break;
+                    default:
+                        Console.WriteLine("Invalid choice!");
+                        break;
+                }
+            }
+        }
+
+        // الدالة لإضافة جهة اتصال جديدة
+        static async Task AddContact()
+        {
+            Console.WriteLine("Enter first name:");
+            string firstName = Console.ReadLine();
+
+            Console.WriteLine("Enter last name:");
+            string lastName = Console.ReadLine();
+
+            Console.WriteLine("Enter email:");
+            string email = Console.ReadLine();
+
+            Console.WriteLine("Enter phone number:");
+            string phoneNumber = Console.ReadLine();
+
+            var newContact = new
+            {
+                contactId = 1,
+                firstName,
+                lastName,
+                email,
+                phoneNumber
             };
 
-            var json = JsonConvert.SerializeObject(contact);
-            var content = new StringContent(json, Encoding.UTF8, "application/json");
+            string url = "https://localhost:7101/api/Contact";
+            var content = new StringContent(JsonConvert.SerializeObject(newContact), System.Text.Encoding.UTF8, "application/json");
 
-            var response = await client.PutAsync("https://localhost:7101/api/Contact", content);
+            var response = await client.PostAsync(url, content);
 
             if (response.IsSuccessStatusCode)
             {
-                Console.WriteLine("Data edit successfully");
-
-                Console.ReadLine(); // هذا السطر يجبر البرنامج على الانتظار قبل الإغلاق
-
+                Console.WriteLine("Data added successfully");
             }
             else
             {
-                // فشل في التحديث
-                var errorMessage = await response.Content.ReadAsStringAsync();
-                Console.WriteLine("Edit operation failed");
-
-                Console.ReadLine(); // هذا السطر يجبر البرنامج على الانتظار قبل الإغلاق
-
-                // إجراءات للتعامل مع الخطأ
+                Console.WriteLine("Add operation failed");
             }
+            Console.ReadLine();
+        }
+        static async Task DeleteContact()
+        {
+            Console.WriteLine("Enter contact ID to delete:");
+            int idToDelete = int.Parse(Console.ReadLine());
 
+            var response = await client.DeleteAsync($"https://localhost:7101/api/Contact/{idToDelete}");
 
-            //نهاية كود التعديل
-
-            //بداية كود الجلب بالرقم
-
-            // رقم جهة الاتصال التي تريد جلب بياناتها
-            /*  int contactId = 1007; // يمكنك تغييرها إلى الرقم المطلوب
-
-              // رابط الطلب لجلب بيانات جهة الاتصال
-              string apiUrl = $"https://localhost:7101/api/Contact/{contactId}";
-
-              // إعداد طلب HTTP GET
-              using (var httpClient = new HttpClient())
-              {
-                  // إرسال طلب HTTP GET
-                  var getResponse = await httpClient.GetAsync(apiUrl);
-
-                  if (getResponse.IsSuccessStatusCode)
-                  {
-                      // قراءة البيانات المسترجعة
-                      var responseBody = await getResponse.Content.ReadAsStringAsync();
-                      Console.WriteLine($"بيانات جهة الاتصال: {responseBody}");
-                      // يمكنك تحويل البيانات النصية إلى كائن Contact هنا إذا كنت ترغب
-                  }
-                  else
-                  {
-                      Console.WriteLine($"فشل في جلب بيانات جهة الاتصال. الرمز الناتج: {getResponse.StatusCode}");
-                      // يمكنك إضافة تعليمات في حالة حدوث أي خطأ أثناء جلب البيانات هنا
-                  }
-              }
-
-              Console.ReadLine(); // هذا السطر يجبر البرنامج على الانتظار قبل الإغلاق
-  */
-            //نهاية كود الجلب بالرقم
-
-
-            //كود التحديث الجزئي>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
-
-
-            // البيانات التي تريد تحديثها (قم بتغيير القيم حسب احتياجاتك)
-            /*  var updatedFields = new
-              {
-                  firstName = "John",
-                  email = "john.doe@example.com"
-              };
-
-              // رقم جهة الاتصال التي تريد تعديل بعض حقولها
-              int contactId = 1007; // قم بتغييرها لتناسب رقم الجهة التي تريد تحديث حقولها
-
-              // رابط الطلب لتحديث البيانات جزئيًا
-              string apiUrl = $"https://localhost:7101/api/Contact/{contactId}"; // قم بتغيير الرابط حسب العنوان الصحيح
-
-              // تحويل البيانات إلى JSON
-              var jsonData = JsonConvert.SerializeObject(updatedFields);
-
-              // إعداد طلب HTTP PATCH
-              using (var httpClient = new HttpClient())
-              {
-                  // إعداد الطلب بنوعية PATCH وتعيين البيانات ورأس الطلب
-                  var patchRequest = new HttpRequestMessage(new HttpMethod("PATCH"), apiUrl);
-                  patchRequest.Content = new StringContent(jsonData, Encoding.UTF8, "application/json");
-
-                  // إرسال طلب HTTP PATCH
-                  var patchResponse = await httpClient.SendAsync(patchRequest);
-
-                  if (patchResponse.IsSuccessStatusCode)
-                  {
-                      Console.WriteLine($"Success");
-                      Console.ReadLine(); // هذا السطر يجبر البرنامج على الانتظار قبل الإغلاق
-
-                      // يمكنك إضافة تعليمات إضافية هنا بعد نجاح التحديث الجزئي
-                  }
-                  else
-                  {
-                      Console.WriteLine($"Fiald {patchResponse.StatusCode}");
-                      Console.ReadLine(); // هذا السطر يجبر البرنامج على الانتظار قبل الإغلاق
-
-                      // يمكنك إضافة تعليمات أو إجراءات إضافية في حالة فشل التحديث الجزئي
-                  }
-              }*/
-
-
+            if (response.IsSuccessStatusCode)
+            {
+                Console.WriteLine($"Data with ID {idToDelete} deleted successfully");
+            }
+            else
+            {
+                Console.WriteLine($"Failed to delete data with ID {idToDelete}. Status code: {response.StatusCode}");
+            }
+            Console.ReadLine();
 
         }
+
+        static async Task UpdateContact()
+        {
+            Console.WriteLine("Enter contact ID to update:");
+            int contactId = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter new first name:");
+            string firstName = Console.ReadLine();
+
+            Console.WriteLine("Enter new last name:");
+            string lastName = Console.ReadLine();
+
+            Console.WriteLine("Enter new email:");
+            string email = Console.ReadLine();
+
+            Console.WriteLine("Enter new phone number:");
+            string phoneNumber = Console.ReadLine();
+
+            var contact = new
+            {
+                ContactId = contactId,
+                FirstName = firstName,
+                LastName = lastName,
+                Email = email,
+                PhoneNumber = phoneNumber
+            };
+
+            var json = JsonConvert.SerializeObject(contact);
+            var content = new StringContent(json, System.Text.Encoding.UTF8, "application/json");
+
+            var response = await client.PutAsync($"https://localhost:7101/api/Contact/{contactId}", content);
+
+            if (response.IsSuccessStatusCode)
+            {
+                Console.WriteLine("Data edited successfully");
+            }
+            else
+            {
+                Console.WriteLine("Edit operation failed");
+            }
+            Console.ReadLine();
+
+        }
+
+        static async Task RetrieveContact()
+        {
+            Console.WriteLine("Enter contact ID to retrieve:");
+            int contactId = int.Parse(Console.ReadLine());
+
+            string apiUrl = $"https://localhost:7101/api/Contact/{contactId}";
+
+            var getResponse = await client.GetAsync(apiUrl);
+
+            if (getResponse.IsSuccessStatusCode)
+            {
+                var responseBody = await getResponse.Content.ReadAsStringAsync();
+                Console.WriteLine($"Contact data: {responseBody}");
+            }
+            else
+            {
+                Console.WriteLine($"Failed to retrieve contact data. Status code: {getResponse.StatusCode}");
+            }
+            Console.ReadLine();
+        }
+       static async Task RetrieveAllContacts()
+{
+    var response = await client.GetAsync("https://localhost:7101/api/Contact");
+
+    if (response.IsSuccessStatusCode)
+    {
+        var responseBody = await response.Content.ReadAsStringAsync();
+        Console.WriteLine($"All Contacts data: {responseBody}");
     }
-
+    else
+    {
+        Console.WriteLine($"Failed to retrieve all contacts. Status code: {response.StatusCode}");
+    }
+    Console.ReadLine();
 }
-/*create database db;
 
 
-use db;
-CREATE TABLE [dbo].[Employees](
-	[EmployeesId] [int] IDENTITY(1,1) NOT NULL,
-	[FirstName] [varchar](200) NOT NULL,
-	[LastName] [varchar](200)NOT NULL,
-	[Email] [varchar](200) NOT NULL,
-	[PhoneNumber] [varchar](200) NOT NULL,
-	)
-drop table Employees;
-INSERT INTO Employees ( FirstName, LastName, Email, PhoneNumber)
-VALUES ( 'mohammed', 'massowd', 'mohammed@massowd.com', '774785445');
 
-INSERT INTO Employees ( FirstName, LastName, Email, PhoneNumber)
-VALUES ('taha', 'massowd', 'taha@massowd.com', '774785446');
-*/
+    }
+}
